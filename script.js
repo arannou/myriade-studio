f011d5e4ecca01c2d7bcf5f7c6b3f8dce5e05240
Alicia Rannou <a.rannou@ateme.com>
2023-09-20 17:40:49 +0200

    Burger menu

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ document.addEventListener('DOMContentLoaded', main, false);
 
 function main() {
     displayCredits()
-    
+
+    document.getElementById("burger").addEventListener('click', showMenu)
+
 }
 
 function displayCredits() {
     const year = new Date().getFullYear();
     document.getElementById("year").innerText = year;
 }
+
+function showMenu() {
+    const list = document.getElementById('menu').classList
+    if (list.contains("open")) {
+        list.remove("open")
+    } else {
+        list.add("open")
+    }
+}
+function hideMenu() {}
