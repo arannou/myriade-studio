76bd8ce4bc417b1b6c94379e41ab5015353f7ec0
Alicia Rannou <a.rannou@ateme.com>
2023-09-23 23:23:47 +0200

    Wip dynamic content

---
+++ content.js
@@ -0,0 +1,35 @@
+const content = [{
+    "type":"menu",
+    "texteFR":"Accueil",
+    "texteEN":"Home",
+    "page": "index.html",
+    "contenu": 
+        [
+            {
+                "type":"texte",
+                "texteFR":"Bonjour",
+                "texteEN":"Bonjour",
+                "position": "left"
+            }
+        ]
+    
+},
+{
+    "type":"menu",
+    "texteFR":"Savoir-faire",
+    "texteEN":"Know-how",
+    "contenu": []
+},
+{
+    "type":"menu",
+    "texteFR":"Réalisations",
+    "texteEN":"Works",
+    "contenu": []
+},
+{
+    "type":"menu",
+    "texteFR":"À propos",
+    "texteEN":"About",
+    "contenu": []
+}
+]
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', main, false);
 
 function main() {
+    const currentPage = displayMenu()
+    displayPage(currentPage)
     displayCredits()
 
     document.getElementById("burger").addEventListener('click', showMenu)
@@ -20,4 +22,64 @@ function showMenu() {
         list.add("open")
     }
 }
-function hideMenu() {}
+
+function displayMenu() {
+    const pathname = window.location.pathname
+    let currentPage;
+    let lu = document.getElementById("meunlu")
+    content.forEach(menu => {
+        let active
+        if (pathname.includes(menu.page)) {
+            active = true
+            currentPage = menu.page
+        } else {
+            active = false
+        }
+
+        let li = document.createElement("li")
+        if (active) {
+            li.classList.add("active")
+        }
+        let a = document.createElement("a");
+        a.setAttribute("href", "./"+menu.page);
+        a.innerText = menu.texteFR
+
+        li.appendChild(a);
+        lu.appendChild(li);
+    })
+    return currentPage
+}
+
+
+function displayImage(media) {
+    let image = document.createElement("img");
+    image.setAttribute("src", media.source);
+    image.setAttribute("alt", media.titre);
+    
+    return image
+}
+
+function displayVideo(media) {
+    let child = document.createElement("iframe");
+    child.src = media.source;
+    child.setAttribute("frameborder", 0);
+    child.setAttribute("allowfullscreen", true);
+    child.width = "560";
+    child.height = "315";
+
+    return child
+}
+
+function displayVideoLocal(media) {
+    child = document.createElement("video");
+    child.setAttribute("controls", true);
+    child.style.width = "560";
+    child.style.height = "315";
+    let source = document.createElement("source");
+    source.src = media.source;
+    source.setAttribute("type", "video/mp4");
+    source.innerText = "Your browser does not support the video tag."
+    child.append(source)
+
+    return child
+}
