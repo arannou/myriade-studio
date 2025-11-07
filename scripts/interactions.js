1f97307fe3c13c74d155ab2fec7db599d7aa665c
Alicia Rannou <a.rannou@ateme.com>
2025-11-07 19:56:31 +0100

    Add english version

--- scripts/interactions.js
+++ scripts/interactions.js
@@ -1,9 +1,49 @@
 document.addEventListener('DOMContentLoaded', interact, false);
 
+let activeLang = "english"
+const allLangs = ["francais", "english"]
+
 function interact() {
     enableCopyMail()
     enableToggleMenu()
 
+    if (navigator.language == 'fr-FR') {
+        activeLang = "francais"
+    }
+
+    switchLang(activeLang)
+
+    document.getElementById("lang-switch-francais").addEventListener("click", () => {
+        if (activeLang == "francais") {
+            return
+        }
+        switchLang("francais")
+    })
+    document.getElementById("lang-switch-english").addEventListener("click", () => {
+        if (activeLang == "english") {
+            return
+        }
+        switchLang("english")
+    })
+}
+
+function switchLang(lang) {
+    activeLang = lang
+    let unActiveLang = allLangs.filter(l => l != lang)[0]
+    // hide elements with class unActiveLang
+    let english = document.getElementsByClassName(unActiveLang)
+    for (let el of english) {
+        el.classList.add("hide")
+    }
+
+    // show elements with class activeLang
+    let francais = document.getElementsByClassName(activeLang)
+    for (let el of francais) {
+        el.classList.remove("hide")
+    }
+
+    document.getElementById("lang-switch-" + activeLang).classList.add("active")
+    document.getElementById("lang-switch-" + unActiveLang).classList.remove("active")
 }
 
 function enableCopyMail() {
